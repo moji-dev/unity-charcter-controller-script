97293829e3534dc3ae7c3fd257ccb3ad919624ea
ARSON <56494633+moji-dev@users.noreply.github.com>
2023-01-01 17:49:23 +0000

    creates wasd controlls

---
+++ CharecterControllerScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharecterControllerScript : MonoBehaviour
+{
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //f
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime; 
+    }
+}
