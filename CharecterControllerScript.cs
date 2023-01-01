267a9ae91f52a8540704aad82726f8734e4f5e36
ARSON <56494633+moji-dev@users.noreply.github.com>
2023-01-01 17:50:15 +0000

    deleted comment

--- CharecterControllerScript.cs
+++ CharecterControllerScript.cs
@@ -8,7 +8,7 @@ public class CharecterControllerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //f
+        //g
     }
 
     // Update is called once per frame
