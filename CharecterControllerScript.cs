b6c95ecca7f3cbc6c8a0d6b776e2ad702bcb28fd
ARSON <56494633+moji-dev@users.noreply.github.com>
2023-01-06 21:04:25 +0000

    added sprint

--- CharecterControllerScript.cs
+++ CharecterControllerScript.cs
@@ -14,15 +14,22 @@ public class CharecterControllerScript : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-         // Calculate the movement direction based on WASD input and the orientation Transform
+{
+    // Calculate the movement direction based on WASD input and the orientation Transform
     Vector3 movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
     movementDirection = orientation.TransformDirection(movementDirection);
 
     // Normalize the movement direction
     movementDirection.Normalize();
 
-    // Apply the movement direction to the player's position
-    transform.Translate(movementDirection * speed * Time.deltaTime);
+    // Increase the movement speed if the left shift key is being pressed
+    float sprintMultiplier = 1.0f;
+    if (Input.GetKey(KeyCode.LeftShift))
+    {
+        sprintMultiplier = 2.0f;
     }
+
+    // Apply the movement direction to the player's position
+    transform.Translate(movementDirection * speed * sprintMultiplier * Time.deltaTime);
+}
 }
